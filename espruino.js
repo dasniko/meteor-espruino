5ccf6ff82e6e657212a23ea3098821ae478b857f
Niko Köbler <niko@n-k.de>
2014-07-16 14:13:29 +0200

    nicer UI

--- espruino.js
+++ espruino.js
@@ -1,9 +1,26 @@
 if (Meteor.isClient) {
     Template.espruino.events({
-        'click input': function (e) {
-            Meteor.call("toggleLed", e.target.id.toUpperCase());
+        'click i.fa': function (e) {
+            var id = e.target.id;
+            Meteor.call("toggleLed", id.toUpperCase(), function(err, result) {
+                console.log(id + ": " + result);
+                Session.set("status_" + id, result);
+            });
         }
     });
+
+    Template.espruino.statusLed1 = function() {
+        return _statusLed("led1");
+    };
+    Template.espruino.statusLed2 = function() {
+        return _statusLed("led2");
+    };
+    Template.espruino.statusLed3 = function() {
+        return _statusLed("led3");
+    };
+    _statusLed = function(led) {
+        return "fa-circle" + (Session.equals("status_" + led, 1) ? "" : "-o");
+    }
 }
 
 if (Meteor.isServer) {
